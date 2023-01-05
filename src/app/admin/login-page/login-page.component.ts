c51422ae74682a01c4878df902df0a0dc61e84de
Thuringud <rom1234593@yandex.ru>
2023-01-05 20:21:11 +0300

    routes protection

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../shared/interfaces";
 import {AuthService} from "../shared/services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -12,13 +12,21 @@ import {Router} from "@angular/router";
 export class LoginPageComponent implements OnInit {
   form!: FormGroup
   submitted = false
+  message: string | undefined
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if(params['needLogin']) {
+        this.message = 'Пожалуйста, войдите повторно'
+      }
+    })
+
     this.form = new FormGroup<any>({
       email: new FormControl(null, [
         Validators.required,
