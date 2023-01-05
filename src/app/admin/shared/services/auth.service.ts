95ab956af7cb866c4785f689b0b158775b8cce0b
Thuringud <rom1234593@yandex.ru>
2023-01-05 17:46:48 +0300

    add error handling

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {User} from "../interfaces";
-import {Observable, tap} from "rxjs";
+import {catchError, Observable, Subject, tap, throwError} from "rxjs";
 import {environment} from "../../../../environments/environment";
 import {FireBaseAuthResponse} from "../../../../environments/interface";
 
 @Injectable()
 
 export class AuthService {
+
+  public error$: Subject<string> = new Subject<string>()
   constructor(private http: HttpClient) {
   }
 
@@ -25,7 +27,8 @@ export class AuthService {
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         // @ts-ignore
-        tap(this.setToken)
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
       )
   }
 
@@ -35,6 +38,24 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return !!this.token
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный адрес электронный почты')
+        break
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный пароль')
+        break
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Адрес электронный почты не найден')
+        break
+    }
+
+    return throwError(error)
   }
 
   private setToken(response: FireBaseAuthResponse | null) {
