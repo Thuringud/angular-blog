d7349d742981aa9129c09483e21807d3cb750826
Thuringud <rom1234593@yandex.ru>
2023-01-06 21:21:48 +0300

    add table in dashboard page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostService} from "../../shared/post.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  postSubscription!: Subscription
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.postSubscription = this.postService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.postSubscription) {
+      this.postSubscription.unsubscribe()
+    }
+  }
+
+  remove(id?: string) {
+
   }
 }
