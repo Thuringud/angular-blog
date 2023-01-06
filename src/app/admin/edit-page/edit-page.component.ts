ab6c4b7620e9cf5e1a1d88728fde84a353908897
Thuringud <rom1234593@yandex.ru>
2023-01-07 01:34:22 +0300

    add post edit page

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostService} from "../../shared/post.service";
+import {switchMap} from "rxjs";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Post} from "../../shared/interfaces";
 
 @Component({
   selector: 'app-edit-page',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPageComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postService.getById(params['id'])
+      })
+    ).subscribe((post: Post) => {
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required),
+      })
+    })
   }
 
+  submit() {
+
+  }
 }
