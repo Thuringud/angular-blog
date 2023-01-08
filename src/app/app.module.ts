fcea93d3a5379b65cade4a879e12448b4332cefe
Thuringud <rom1234593@yandex.ru>
2023-01-08 20:56:43 +0300

    update pwa settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptor} from "./shared/auth.interceptor";
 import {registerLocaleData} from "@angular/common";
 import ruLocale from "@angular/common/locales/ru";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { PostComponent } from './shared/components/post/post.component';
 import {SharedModule} from "./shared/shared.module";
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 registerLocaleData(ruLocale, 'ru')
 
