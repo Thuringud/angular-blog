927f293f64c11031069f990d689bd3e9892b0d2f
Thuringud <rom1234593@yandex.ru>
2023-01-09 23:34:20 +0300

    add comment in environments files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyBaxPjMTh1dxFBoTC65V311ImVuQ8wJcOI',
+  apiKey: 'AIzaSyBaxPjMTh1dxFBoTC65V311ImVuQ8wJcOI', // оставлен намеренно, не имеет ценности
   FireBaseDBurl: 'https://angular-blog-8e3c6-default-rtdb.asia-southeast1.firebasedatabase.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyBaxPjMTh1dxFBoTC65V311ImVuQ8wJcOI',
+  apiKey: 'AIzaSyBaxPjMTh1dxFBoTC65V311ImVuQ8wJcOI', // оставлен намеренно, не имеет ценности
   FireBaseDBurl: 'https://angular-blog-8e3c6-default-rtdb.asia-southeast1.firebasedatabase.app'
 };
 
